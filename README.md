cb63c3156852b99ddd2f6215791490e26972982c
Zazimko Alexey <notfoundsam@gmail.com>
2017-12-08 12:44:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JpPrefecture
---
+++ src/Prefecture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace JpPrefecture;
+
+/**
+ * 
+ * 
+ * @category Prefecture
+ * @package  JpPrefecture\Prefecture
+ * @author   Zazimko Alexey <notfoundsam@gmail.com>
+ * @link     https://github.com/notfoundsam/jp_prefecture
+ *
+ */
+class Prefecture
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function find()
+    {
+        
+    }
+}
